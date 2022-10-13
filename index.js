06f3d82d6fbbbff36f91fe522135c8239379fbac
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-12 19:01:31 -0500

    finishes the recursive function to read dir

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const isMd = 'README.md';
-const folder = '/Users/lala/Documents/Laboratoria/Proyecto4 - MDLinks/CDMX013-md-links'
+const folder = '/Users/lala/Documents/Laboratoria/Proyecto4 - MDLinks/CDMX013-md-links';
 // Read a file
 
 /* fs.readFile(isMd, 'utf8', (err, data) => {
@@ -10,26 +10,13 @@ const folder = '/Users/lala/Documents/Laboratoria/Proyecto4 - MDLinks/CDMX013-md
     return;
   }
   console.log(data);
-});  */
+}); */
 
 // Obtain the file extension
 
 // console.log(path.extname(isMd));
 
 // Obtain the directory content
-
-/* const traverseSync = dir => ({
-  path: dir,
-  children: fs.readdirSync(dir).map(file => {
-    const pathW = path.join(dir, file);
-    return fs.lstatSync(pathW).isDirectory()
-      ? traverseSync(pathW)
-      : { path };
-  })
-});
-
-console.log(traverseSync(folder)) */
-
 // read directory and tells the filenames
   // go through the dir and gives you the .md file
 
@@ -51,10 +38,31 @@ console.log(traverseSync(folder)) */
     }
   }) */
 
-
 /* filenames = fs.readdirSync(folder);
   
 console.log("\nCurrent directory filenames:");
 filenames.forEach(file => {
   console.log(file);
-});  */
+}); */
+
+
+const getFileList = (dirName) => {
+  let files = [];
+  const items = fs.readdirSync(dirName, { withFileTypes: true });
+
+  items.forEach((item) => {
+    const pathW = path.join(dirName, item.name);
+      if (item.isDirectory()) {
+          files = [...files, ...getFileList(pathW)];
+      } else {
+          files.push(pathW);
+      }
+  })
+  return files;
+  /* const mdFiles = files.map((file) => (path.extname(file) == ".md"));
+  return mdFiles; */
+};
+
+const files = getFileList(folder);
+
+console.log(files);
