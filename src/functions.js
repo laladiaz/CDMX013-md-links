e97df09fbb72959ac1ad4f0c97b58aed75bb88e0
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-14 18:03:18 -0500

    moves to fuctions.js the functions of path and fs

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+// Checks if the path is absolute
+const isAbsolutePath = (dirName) => path.isAbsolute(dirName);
+// Resolve the path to make it absolute
+const resolvePath = (dirName) => path.resolve(dirName);
+// Checks if the path given is directory
+const isAFolder = (dirName) => fs.lstatSync(dirName).isDirectory();
 // Recursive function to get the content of a directory
 const folderContent = (dirName) => {
     let files = [];
@@ -11,8 +17,16 @@ const folderContent = (dirName) => {
     });
     return files;
   }
+  // get the .md file
+ const mdExtName = (dirName) => path.extname(dirName);
+ // read file
+ const readMdFile = (dirName) => fs.readFileSync(dirName, 'utf-8');
 
-/* module.exports = {
+module.exports = {
     folderContent,
-
-} */
+    isAbsolutePath,
+    resolvePath,
+    isAFolder,
+    mdExtName,
+    readMdFile
+}
