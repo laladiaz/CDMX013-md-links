1fd9f4b0e136aa11d9254ed8494daa2d3b9102ad
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-18 18:15:37 -0500

    recives the functions from api.js into fct.js

--- src/fct.js
+++ src/fct.js
@@ -9,24 +9,47 @@ const resolvePath = (dirName) => path.resolve(dirName);
 const isAFolder = (dirName) => fs.lstatSync(dirName).isDirectory();
 // Recursive function to get the content of a directory
 const folderContent = (dirName) => {
-    let files = [];
-    const items = fs.readdirSync(dirName, { withFileTypes: true });
-    items.forEach((item) => {
-      const pathW = path.join(dirName, item.name);
-      item.isDirectory() ? files = [...files, ...folderContent(pathW)] : files.push(pathW);
-    });
-    return files;
-  }
-  // get the .md file
- const mdExtName = (dirName) => path.extname(dirName);
- // read file
- const readMdFile = (dirName) => fs.readFileSync(dirName, 'utf-8');
+  let files = [];
+  const items = fs.readdirSync(dirName, { withFileTypes: true });
+  items.forEach((item) => {
+    const pathW = path.join(dirName, item.name);
+    item.isDirectory() ? files = [...files, ...folderContent(pathW)] : files.push(pathW);
+  });
+  return files;
+}
+// get the .md file
+const filteredArray = (dirPath) => dirPath.filter(file => (path.extname(file) == ".md"));
+//const mdExtName = (dirName) => path.extname(dirName);
+// read file
+// const readMdFile = (dirName) => fs.readFileSync(dirName, 'utf-8');
+// get the links in .md
+const getLinks = (arrLinks) => {
+  let links = [];
+  arrLinks.forEach((file) => {
+    // read the files
+    const content = fs.readFileSync(file, 'utf-8');
+    // obtain the urls
+    const regExp = /\[(.+)\]\((https?:\/\/.+)\)/gi;
+    const linksFound = [...content.matchAll(regExp)];
+    if (linksFound !== null || linksFound.length !== 0) {
+      // ensambles the array of objects
+      linksFound.forEach(link => {
+        links.push({
+          file: file,
+          href: link[2],
+          text: link[1]
+        })
+      })
+    }
+  })
+  return links
+}
 
 module.exports = {
-    folderContent,
-    isAbsolutePath,
-    resolvePath,
-    isAFolder,
-    mdExtName,
-    readMdFile
+  folderContent,
+  isAbsolutePath,
+  resolvePath,
+  isAFolder,
+  filteredArray,
+  getLinks
 }
