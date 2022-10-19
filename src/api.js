73426e749ea5d67538be4667fdeda30bd9bd2edd
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-19 18:01:45 -0500

    adds the promise and the if to start workin on validation in api.js

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
 const fct = require('./fct.js');
 
-const mdLinks = (filePath) => {
-  // create the return promise function
-  //check if filePath is absolute or relative
-  const absolutePath = fct.isAbsolutePath(filePath) ? filePath : fct.resolvePath(filePath);
-  // if the path given is a directory, get the directory content, if the path given is a file, does not enter the recursive function
-  let dirFiles = [];
-  fct.isAFolder(absolutePath) ? dirFiles = [...dirFiles, ...fct.folderContent(absolutePath)] : dirFiles.push(absolutePath);
-  // get the .md file
-  const fileArray = fct.filteredArray(dirFiles);
-  // array of links
-  const allLinks = fct.getLinks(fileArray);
+const mdLinks = (filePath, opt) => new Promise((resolve, reject) => {
+    //check if filePath is absolute or relative
+    const absolutePath = fct.isAbsolutePath(filePath) ? filePath : fct.resolvePath(filePath);
+    // if the path given is a directory, get the directory content, if the path given is a file, does not enter the recursive function
+    let dirFiles = [];
+    fct.isAFolder(absolutePath) ? dirFiles = [...dirFiles, ...fct.folderContent(absolutePath)] : dirFiles.push(absolutePath);
+    // get the .md file
+    const fileArray = fct.filteredArray(dirFiles);
+    // array of links
+    const allLinks = fct.getLinks(fileArray);
 
-  return allLinks;
-};
+    resolve(allLinks);
+    /* if (opt.validate){fetch}
+    else{resolve(allLinks);} */
+  });
 
 module.exports = {
   mdLinks
