3a72313e8270cbbbf95ac2a1199eec905a4df41f
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-21 18:05:12 -0500

    starts writing the function to validate links in api.js

--- src/api.js
+++ src/api.js
@@ -1,20 +1,54 @@
 const fct = require('./fct.js');
+const axios = require('axios');
 
 const mdLinks = (filePath, opt) => new Promise((resolve, reject) => {
-    //check if filePath is absolute or relative
-    const absolutePath = fct.isAbsolutePath(filePath) ? filePath : fct.resolvePath(filePath);
-    // if the path given is a directory, get the directory content, if the path given is a file, does not enter the recursive function
-    let dirFiles = [];
-    fct.isAFolder(absolutePath) ? dirFiles = [...dirFiles, ...fct.folderContent(absolutePath)] : dirFiles.push(absolutePath);
-    // get the .md file
-    const fileArray = fct.filteredArray(dirFiles);
-    // array of links
-    const allLinks = fct.getLinks(fileArray);
+  //check if filePath is absolute or relative
+  const absolutePath = fct.isAbsolutePath(filePath) ? filePath : fct.resolvePath(filePath);
+  // if the path given is a directory, get the directory content, if the path given is a file, does not enter the recursive function
+  let dirFiles = [];
+  fct.isAFolder(absolutePath) ? dirFiles = [...dirFiles, ...fct.folderContent(absolutePath)] : dirFiles.push(absolutePath);
+  // get the .md file
+  const fileArray = fct.filteredArray(dirFiles);
+  // array of links
+  const allLinks = fct.getLinks(fileArray);
 
+  // resolve(allLinks);
+  if (opt.validate === true) {
+    const newArray = fct.arrayOfLinks(allLinks);
+
+    let arrayOfPromises = [];
+    newArray.forEach((link) => {
+      let promiseLink = axios.get(link)
+        .then((result) => {
+          return {
+            status: result.status,
+            message: result.statusText
+          }
+        })
+        .catch((error) => {
+        if (error.response) {
+          return {
+            href: link,
+            status: error.response.status,
+            message: error.response.statusText
+          };
+        } else{
+          return {
+            status: error.message,
+            message: 'fail'
+          }
+        }
+      });
+      arrayOfPromises.push(promiseLink);
+    })
+    Promise.all(arrayOfPromises)
+      .then((response) => resolve(response))
+    // resolve(granPromise);
+
+  } else {
     resolve(allLinks);
-    /* if (opt.validate){fetch}
-    else{resolve(allLinks);} */
-  });
+  }
+});
 
 module.exports = {
   mdLinks
