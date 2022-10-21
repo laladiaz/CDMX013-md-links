ada674ad55b2e021bede34cc82d0f731dd9de599
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-20 19:51:03 -0500

    test the axios.get from fct in index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const api = require('./src/api.js')
+const fct = require('./src/fct.js');
 const isMdRelative = 'README.md';
 const isMd = '/Users/lala/Documents/Laboratoria/Proyecto1-cipher/CDMX013-cipher/README.md'
 const folder = '/Users/lala/Documents/Laboratoria/Proyecto4 - MDLinks/CDMX013-md-links';
 const folderRelative = './test';
+const URL = 'https://axios-http.com/docs/notes';
 
 
-console.log(api.mdLinks(folderRelative));
+// console.log(api.mdLinks(folderRelative));
+fct.testGET(URL)
+.then(response => console.log(response))
