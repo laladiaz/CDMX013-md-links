22ca68959391eca6d5ab5e8022d22cbb1d99fe72
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-24 17:06:08 -0500

    gets the promises function from fct.js in api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
 const fct = require('./fct.js');
-const axios = require('axios');
 
 const mdLinks = (filePath, opt) => new Promise((resolve) => {
   //check if filePath is absolute or relative
@@ -13,42 +12,13 @@ const mdLinks = (filePath, opt) => new Promise((resolve) => {
   const allLinks = fct.getLinks(fileArray);
 
   if (opt.validate === true) {
-    // get an array of only links URL
-    const newArray = fct.arrayOfLinks(allLinks);
-    // axios.get for every link
-    let arrayOfPromises = [];
-    newArray.forEach((link) => {
-      let promiseLink = axios.get(link)
-        .then((result) => {
-          return {
-            href: link,
-            text: allLinks.text,
-            file: allLinks.file,
-            status: result.status,
-            message: result.statusText
-          }
-        })
-        .catch((error) => {
-        if (error.response) {
-          return {
-            href: link,
-            status: error.response.status,
-            message: error.response.statusText
-          };
-        } else{
-          return {
-            status: error.message,
-            message: 'fail'
-          }
-        }
-      });
-      arrayOfPromises.push(promiseLink);
-    })
-    return Promise.all(arrayOfPromises)
-     .then((result) => resolve(result));
+       // axios.get for every link
+   const promisesArrayToResolve = fct.promises(allLinks);
+    return Promise.all(promisesArrayToResolve)
+      .then((result) => resolve(result));
 
   } else {
-    resolve(allLinks);
+    return resolve(allLinks);
   }
 });
 
