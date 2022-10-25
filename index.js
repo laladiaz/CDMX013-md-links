5cdd43c3eaecfee5424e9dbeaed5c3ec5d8ed341
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-25 08:48:53 -0500

    test mdLinks with validate: true in index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const URLFail = 'http://community.laboratoria.la/t/modulos-librerias-paquetes-fr
 const URLFail2 = 'https://developer.mozilla.org/e/docs/Web/JavaScript/Reference/Global_Objects/Set';
 
 
-api.mdLinks(folderRelative, {validate: false}).then(console.log);
-// api.mdLinks(folderRelative, {validate: true}).then(console.log);
+// api.mdLinks(folderRelative, {validate: false}).then(console.log);
+api.mdLinks(folderRelative, {validate: true}).then(console.log);
