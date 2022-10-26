11f1a0e68370148ff89e984ad2fd548524f15198
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-26 13:11:54 -0500

    tries to improve the code using DRY in api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const fct = require('./lib/fct.js');
 
+
 const mdLinks = (filePath, opt) => new Promise((resolve) => {
   //check if filePath is absolute or relative
   const absolutePath = fct.isAbsolutePath(filePath) ? filePath : fct.resolvePath(filePath);
@@ -9,10 +10,26 @@ const mdLinks = (filePath, opt) => new Promise((resolve) => {
   // get the .md file
   const fileArray = fct.filteredArray(dirFiles);
   // array of links
-  const allLinks = fct.getLinks(fileArray);
+  let links = [];
+  fileArray.forEach((file) => {
+   // read the files
+    const content = fct.readFile(file);
+    // obtain the urls
+    const linksFound = fct.regexGetLinks(content);
+    if (linksFound !== null || linksFound.length !== 0) {
+      // ensambles the array of objects
+      linksFound.forEach(link => {
+        return links.push({
+          file: file,
+          href: link[2],
+          text: link[1].slice(0, 50)
+        })
+      })
+    }
+  })
   if (opt.validate === true) {
        // axios.get for every link
-   const promisesArrayToResolve = fct.promises(allLinks);
+   const promisesArrayToResolve = fct.promises(links);
     return Promise.all(promisesArrayToResolve)
       .then((result) => resolve(result));
   } else {
