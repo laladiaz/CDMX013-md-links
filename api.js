7406f8f84d6acdd637eea99d79439f5e8582b35b
laladiaz <mariaabelardadiaz@gmail.com>
2022-10-27 05:46:44 -0500

    reverts api.js as was in v1.0

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 const fct = require('./lib/fct.js');
 
-
 const mdLinks = (filePath, opt) => new Promise((resolve) => {
   //check if filePath is absolute or relative
   const absolutePath = fct.isAbsolutePath(filePath) ? filePath : fct.resolvePath(filePath);
@@ -10,30 +9,16 @@ const mdLinks = (filePath, opt) => new Promise((resolve) => {
   // get the .md file
   const fileArray = fct.filteredArray(dirFiles);
   // array of links
-  let links = [];
-  fileArray.forEach((file) => {
-   // read the files
-    const content = fct.readFile(file);
-    // obtain the urls
-    const linksFound = fct.regexGetLinks(content);
-    if (linksFound !== null || linksFound.length !== 0) {
-      // ensambles the array of objects
-      linksFound.forEach(link => {
-        return links.push({
-          file: file,
-          href: link[2],
-          text: link[1].slice(0, 50)
-        })
-      })
-    }
-  })
+  const allLinks = fct.getLinks(fileArray);
+
   if (opt.validate === true) {
        // axios.get for every link
-   const promisesArrayToResolve = fct.promises(links);
+   const promisesArrayToResolve = fct.promises(allLinks);
     return Promise.all(promisesArrayToResolve)
       .then((result) => resolve(result));
+
   } else {
-    return resolve(links);
+    return resolve(allLinks);
   }
 });
 
