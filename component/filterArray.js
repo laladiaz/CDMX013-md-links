a57c9e495f3bc35aa319ef578964a3411a0f7c52
laladiaz <mariaabelardadiaz@gmail.com>
2022-11-03 18:53:45 -0600

    corrects the strict equal in filterArray.js

--- component/filterArray.js
+++ component/filterArray.js
@@ -1,7 +1,7 @@
 const { extensionName } = require('../lib/fromPath.js');
 
 // get the .md file
-const filteredArray = (dirPath) => dirPath.filter(file => (extensionName(file) == ".md"));
+const filteredArray = (dirPath) => dirPath.filter(file => (extensionName(file) === ".md"));
 
 module.exports = {
     filteredArray
